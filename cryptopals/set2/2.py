004dd73a9d963ec752528e3e761fa2fe2c4627b1
Lucas Rival <lucas.rival@gmail.com>
2016-11-02 14:00:20 +0100

    added the challange 2 solution

--- cryptopals/set2/2.py
+++ cryptopals/set2/2.py
@@ -8,27 +8,52 @@ from Crypto import Random
 from cryptopals import *
 
 BLOCK_SIZE=16
-string="Il n'acheva point. Une seconde balle du meme tireur l'arreta court. Cette fois il s'abattit la face contre le pave, et ne remua plus. Cette petite grande ame venait de s'envoler."
+string=b"Il n'acheva point. Une seconde balle du meme tireur l'arreta court. Cette fois il s'abattit la face contre le pave, et ne remua plus. Cette petite grande ame venait de s'envoler."
 key=b'YELLOW SUBMARINE'
+
+
+def xor(a,b):
+    assert(len(a)==len(b))
+    result=bytearray()
+    for c in range(len(a)):
+        result.append(a[c]^b[c])
+    return bytes(result)
+
 
 def encrypt(string,key,mode):
     encryptor=AES.new(key,mode)
-    print(len(string),len(key))
     return encryptor.encrypt(string)
 
 def decrypt(string,key,mod):
     decryptor=AES.new(key)
     return decryptor.decrypt(string)
 
-encryptedString=b''
-for i in range(int(len(string)/16)):
-    block=string[i*16:(i+1)*16]
-    if i==0:
-        IV=b'\x00'*BLOCK_SIZE
-    else:
-        IV=blockEncrypted
-    blockXor=xor(block.encode(),IV)
-    blockEncrypted=encrypt(blockXor,key,AES.MODE_ECB)
-    # print(block, IV, blockEncrypted)
-    encryptedString+=blockEncrypted
-print(encryptedString)
+def encryptCBC(string,key,IV):
+    encryptedString=b''
+    for i in range(int(len(string)/16)):
+        block=string[i*16:(i+1)*16]
+        if i<0:
+            IV=blockEncrypted
+        blockXor=xor(block,IV)
+        blockEncrypted=encrypt(blockXor,key,AES.MODE_ECB)
+        encryptedString+=blockEncrypted
+    return encryptedString
+
+
+def decryptCBC(string,key,IV):
+    decryptedString=b''
+    for i in range(int(len(string)/16)):
+        block=string[i*16:(i+1)*16]
+        if i<0:
+            IV=lastBlock
+        if len(block)<16:
+            block=genPadding(block,16)
+        blockDecrypted=decrypt(block,key,AES.MODE_ECB)
+        blockXor=xor(blockDecrypted,IV)
+        decryptedString+=blockXor
+        lastBlock=block
+    return decryptedString
+
+IV=b'\x00'*BLOCK_SIZE
+print(encryptCBC(string,key,IV))
+print(decryptCBC(encryptCBC(string,key,IV),key,IV))
